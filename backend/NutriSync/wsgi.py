06f599a2f3fd396b6ad2bb51e586fb505e29e731
Jorgeimlz <jorge.moncayo.loaiza@udla.edu.ec>
2024-11-09 18:32:07 -0500

    QUIZAAAAAAA

--- backend/NutriSync/wsgi.py
+++ backend/NutriSync/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+# NutriSync/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NutriSync.settings')
